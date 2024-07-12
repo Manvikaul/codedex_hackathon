86bad35845e4c5008663d73ab5943730bf805dc2
mangswe <mangcin9@gmail.com>
2024-07-12 01:08:51 -0500

    Components

--- cafe_website/src/App.js
+++ cafe_website/src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import './App.css';
+import Home from './Components/Home';
+import Menu from './Components/Menu';
+import Schedule from './Components/Schedule';
+import BoardGames from './Components/Board Games';
 
 
 function App() {
   return (
     <div className="App">
       
+      <Home />
+      <Menu />
+      <Schedule />
+      <BoardGames />
     </div>
   );
 }
---
+++ cafe_website/src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#menu">Menu</a></li>
+        <li><a href="#schedule">Schedule</a></li>
+        <li><a href="#boardgames">Board Games</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
